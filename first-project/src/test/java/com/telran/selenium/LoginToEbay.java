461dd2006ba677432f28e06a925fb73963982d3e
oklevtsova <54117576+oklevtsova@users.noreply.github.com>
2019-08-28 19:05:25 +0300

    Added methods

---
+++ first-project/src/test/java/com/telran/selenium/LoginToEbay.java
@@ -0,0 +1,28 @@
+package com.telran.selenium;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class LoginToEbay extends TestBase {
+    @Test
+    public void logInTest(){
+        driver.get("https://ebay.com");
+
+        click(By.id("gh-ug"));
+
+        click(By.id("userid"));
+        driver.findElement(By.id("userid")).clear();
+        driver.findElement(By.id("userid")).sendKeys("asdfg@fghj.com");
+
+        click(By.id("pass"));
+        driver.findElement(By.id("pass")).clear();
+        driver.findElement(By.id("pass")).sendKeys("123456789");
+
+        click(By.id("sgnBt"));
+
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+}
--- first-project/src/test/java/com/telran/selenium/OpenGoogleTest.java
+++ first-project/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -1,21 +1,27 @@
 package com.telran.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class OpenGoogleTest {
-    WebDriver driver;
+import java.util.concurrent.TimeUnit;
+
+public class OpenGoogleTest extends TestBase {
 
     @Test
-    public void setUp() throws InterruptedException {
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        public void searchJavaTest() throws InterruptedException {
+            driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("java"+ Keys.ENTER);
 
-        driver.findElement(By.name("q")).sendKeys("java");
 
-        Thread.sleep(7000);
-        driver.quit();
-    }
+
+            Thread.sleep(7000);
+        }
 }
+
