7486a6c73d532dcaad201d62893b1ed74227bbf1
oklevtsova <54117576+oklevtsova@users.noreply.github.com>
2019-08-28 14:14:36 +0300

    Added method Open Google

---
+++ first-project/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -0,0 +1,21 @@
+package com.telran.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogleTest {
+    WebDriver driver;
+
+    @Test
+    public void setUp() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        driver.findElement(By.name("q")).sendKeys("java");
+
+        Thread.sleep(7000);
+        driver.quit();
+    }
+}
